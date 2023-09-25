5b3e147a05f5781224ff7be6ca560e3ce8058ed9
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-25 21:25:24 +0530

    components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import {
   headTextAnimation,
   slideAnimation,
 } from "../config/motion";
+import { CustomButton } from "../components";
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -28,13 +29,22 @@ const Home = () => {
                 LET'S <br className="block-hidden" /> DO IT.
               </h1>
             </motion.div>
-            <motion.div>
-              <p className="max-w-md font-normal text-gray-500">
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-500 text-base">
                 Create your unique and exclusive shirt with our brand-new 3d
                 customization tool.
                 <strong> Unleash your Imagination </strong> and define your own
                 style.
               </p>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.div>
